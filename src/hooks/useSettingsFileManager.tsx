59d93aba8049030cf3cb62a10995b0e70efeac5f
steve1316 <tustev00@gmail.com>
2025-10-30 10:45:25 -0700

    Remove unused import

--- src/hooks/useSettingsFileManager.tsx
+++ src/hooks/useSettingsFileManager.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react"
+import { useState } from "react"
 import * as DocumentPicker from "expo-document-picker"
 import * as Sharing from "expo-sharing"
 import { useSettings } from "../context/SettingsContext"
