d18442bd3037f9c4ce47f37aba10d433edd9e487
steve1316 <tustev00@gmail.com>
2025-11-11 15:37:06 -0800

    Add the new character and support event overrides to the MessageLog

--- src/components/MessageLog/index.tsx
+++ src/components/MessageLog/index.tsx
@@ -169,6 +169,16 @@ const MessageLog = () => {
                 ? "No Special Event Overrides"
                 : `${Object.keys(settings.trainingEvent.specialEventOverrides).length} Special Event Overrides applied`
         }
+👤 Character Event Overrides: ${
+            Object.keys(settings.trainingEvent.characterEventOverrides).length === 0
+                ? "No Character Event Overrides"
+                : `${Object.keys(settings.trainingEvent.characterEventOverrides).length} Character Event Override(s) applied`
+        }
+💪 Support Event Overrides: ${
+            Object.keys(settings.trainingEvent.supportEventOverrides).length === 0
+                ? "No Support Event Overrides"
+                : `${Object.keys(settings.trainingEvent.supportEventOverrides).length} Support Event Override(s) applied`
+        }
 🔋 Prioritize Energy Options: ${settings.trainingEvent.enablePrioritizeEnergyOptions ? "✅" : "❌"}
 
 ---------- Training Options ----------
