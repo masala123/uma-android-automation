fe51bc9e67a8661b511d9302659ab2a43ac4cd02
steve1316 <tustev00@gmail.com>
2025-11-14 19:38:53 -0800

    Prevent potential ENOENT errors coming from Metro for the Android build files

--- metro.config.js
+++ metro.config.js
@@ -15,4 +15,14 @@ config.resolver.alias = {
     "@": path.resolve(__dirname, "./"),
 }
 
+// Exclude Android build directories from Metro's file watcher to prevent ENOENT errors on Windows.
+// These directories are created dynamically by Gradle and can cause Metro to crash when trying to watch them.
+config.watchFolders = config.watchFolders || []
+config.resolver = config.resolver || {}
+config.resolver.blockList = [
+    // Exclude Android build directories.
+    /android\/app\/build\/.*/,
+    /android\/build\/.*/,
+]
+
 module.exports = withNativeWind(config, { input: "./global.css", inlineRem: 16 })
