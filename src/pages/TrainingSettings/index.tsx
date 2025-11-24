85017b2f6f0b7e9ba47376961135a600f19618a8
steve1316 <tustev00@gmail.com>
2025-11-23 16:52:12 -0800

    Apply formatting

--- src/pages/TrainingSettings/index.tsx
+++ src/pages/TrainingSettings/index.tsx
@@ -363,9 +363,7 @@ const TrainingSettings = () => {
 
                     {bsc.settings.general.scenario === "Unity Cup" && (
                         <View style={styles.errorContainer}>
-                            <Text style={styles.errorText}>
-                                ⚠️ Unity Cup Note: Unity trainings will take priority over stat prioritization up till Senior Year.
-                            </Text>
+                            <Text style={styles.errorText}>⚠️ Unity Cup Note: Unity trainings will take priority over stat prioritization up till Senior Year.</Text>
                         </View>
                     )}
 
@@ -490,7 +488,8 @@ const TrainingSettings = () => {
                         <Text style={[styles.label, { fontSize: 14, color: colors.foreground, opacity: 0.7, marginTop: 4 }]}>
                             Set the preferred race distance for training targets. "Auto" will automatically determine based on character aptitudes reading from left to right (S {">"} A priority).
                             {"\n\n"}
-                            For example, if Gold Ship has an aptitude of A for both Medium and Long, Auto will use Medium as the preferred distance. Whereas if Medium is A and Long is S, then Auto will instead use Long as the preferred distance.
+                            For example, if Gold Ship has an aptitude of A for both Medium and Long, Auto will use Medium as the preferred distance. Whereas if Medium is A and Long is S, then Auto
+                            will instead use Long as the preferred distance.
                         </Text>
                     </View>
 
