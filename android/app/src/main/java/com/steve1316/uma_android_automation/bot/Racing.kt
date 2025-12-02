395de8a0ecedac4f55c2e5688b79975684cbaf34
Sj-Si <sjw.jetty@gmail.com>
2025-12-02 09:37:51 -0500

    Fix race strategy when strategy override is disabled.

--- android/app/src/main/java/com/steve1316/uma_android_automation/bot/Racing.kt
+++ android/app/src/main/java/com/steve1316/uma_android_automation/bot/Racing.kt
@@ -139,6 +139,8 @@ class Racing (private val game: Game) {
 
                 var runningStyle: RunningStyle? = null
                 val runningStyleString: String = when {
+                    // If race strategy override is disabled, just use the AUTO strategy.
+                    !enableRaceStrategyOverride -> "AUTO"
                     // Special case for when the bot has not been able to check the date
                     // i.e. when the bot starts at the race screen.
                     game.currentDate.day == 1 -> userSelectedOriginalStrategy
