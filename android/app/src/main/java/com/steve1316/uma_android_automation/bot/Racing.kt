masala123/uma-android-automation

commit 36f768446163ea9ac36271b567831f4a198900ba

Fix potential deadlock.

android/app/src/main/java/com/steve1316/uma_android_automation/bot/Racing.kt
@@ -1128,15 +1128,17 @@ class Racing (private val game: Game) {
             // at the race screen. We need to open the race strategy dialog and
             // read the aptitudes in from there.
             MessageLog.i(TAG, "Setting running style and performing temporary initial aptitude check.")
-            while (!handleDialogs().first) {
-                ButtonChangeRunningStyle.click(imageUtils = game.imageUtils)
-            }
+            ButtonChangeRunningStyle.click(imageUtils = game.imageUtils, tries = 10)
+            game.wait(0.5, skipWaitingForLoading = true)
+            var tries = 10
+            while (tries > 0 && !handleDialogs().first) {}
         } else if (!game.trainee.bHasSetRunningStyle) {
             // If we haven't set the trainee's running style yet, open the dialog.
             MessageLog.i(TAG, "Setting running style for the first time.")
-            while (!handleDialogs().first) {
-                ButtonChangeRunningStyle.click(imageUtils = game.imageUtils)
-            }
+            ButtonChangeRunningStyle.click(imageUtils = game.imageUtils, tries = 10)
+            game.wait(0.5, skipWaitingForLoading = true)
+            var tries = 10
+            while (tries > 0 && !handleDialogs().first) {}
         }
     }
 

commit 3f5b3155d9ec538c04081aca7e4b94ad371c1e6b

Fix infinite loop introduced by 36f768446163ea9ac36271b567831f4a198900ba
- The counter was never decremented.

android/app/src/main/java/com/steve1316/uma_android_automation/bot/Racing.kt
@@ -1485,14 +1485,18 @@ class Racing (private val game: Game) {
             ButtonChangeRunningStyle.click(imageUtils = game.imageUtils, tries = 10)
             game.wait(0.5, skipWaitingForLoading = true)
             var tries = 10
-            while (tries > 0 && !handleDialogs().first) {}
+            while (tries > 0 && !handleDialogs().first) {
+                tries--
+            }
         } else if (!game.trainee.bHasSetRunningStyle) {
             // If we haven't set the trainee's running style yet, open the dialog.
             MessageLog.i(TAG, "Setting running style for the first time.")
             ButtonChangeRunningStyle.click(imageUtils = game.imageUtils, tries = 10)
             game.wait(0.5, skipWaitingForLoading = true)
             var tries = 10
-            while (tries > 0 && !handleDialogs().first) {}
+            while (tries > 0 && !handleDialogs().first) {
+                tries--
+            }
         }
     }
 