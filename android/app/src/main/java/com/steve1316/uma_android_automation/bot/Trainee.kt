cb92fb0501fd2cba8a50f5645961570cff806fbd
Sj-Si <sjw.jetty@gmail.com>
2026-01-14 01:50:56 -0500

    Fix missing logger tag to Trainee object.

--- android/app/src/main/java/com/steve1316/uma_android_automation/bot/Trainee.kt
+++ android/app/src/main/java/com/steve1316/uma_android_automation/bot/Trainee.kt
@@ -12,6 +12,7 @@ import java.util.concurrent.TimeUnit
 import com.steve1316.automation_library.utils.MessageLog
 import com.steve1316.automation_library.utils.BotService
 
+import com.steve1316.uma_android_automation.MainActivity
 import com.steve1316.uma_android_automation.utils.SettingsHelper
 import com.steve1316.uma_android_automation.utils.CustomImageUtils
 import com.steve1316.uma_android_automation.utils.types.StatName
@@ -33,7 +34,7 @@ import com.steve1316.uma_android_automation.components.LabelStatStyle
 
 class Trainee {
     companion object {
-        const val TAG: String = "Trainee"
+        const val TAG: String = "[${MainActivity.Companion.loggerTag}]Trainee"
 
         data class Stats(
             var speed: Int = -1,
