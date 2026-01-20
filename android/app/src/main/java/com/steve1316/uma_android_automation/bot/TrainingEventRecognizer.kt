11e9d0ca0177a135620cce570b58a5b46a37e07f
steve1316 <tustev00@gmail.com>
2026-01-20 11:38:03 -0800

    Update project to reference the framework's SettingsHelper and SQLiteSettingsManager

--- android/app/src/main/java/com/steve1316/uma_android_automation/bot/TrainingEventRecognizer.kt
+++ android/app/src/main/java/com/steve1316/uma_android_automation/bot/TrainingEventRecognizer.kt
@@ -3,8 +3,8 @@ package com.steve1316.uma_android_automation.bot
 import android.util.Log
 import com.steve1316.uma_android_automation.MainActivity
 import com.steve1316.uma_android_automation.utils.CustomImageUtils
-import com.steve1316.uma_android_automation.utils.SettingsHelper
-import com.steve1316.uma_android_automation.utils.SQLiteSettingsManager
+import com.steve1316.automation_library.utils.SettingsHelper
+import com.steve1316.automation_library.utils.SQLiteSettingsManager
 import com.steve1316.automation_library.utils.MessageLog
 import net.ricecode.similarity.JaroWinklerStrategy
 import net.ricecode.similarity.StringSimilarityServiceImpl
