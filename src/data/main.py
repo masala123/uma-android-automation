990e97bd1ee183b5aab77c6d558469c0edf6afdb
Sj-Si <sjw.jetty@gmail.com>
2026-01-26 09:33:56 -0500

    Add condition string to skills data. Run scraper.

--- src/data/main.py
+++ src/data/main.py
@@ -655,6 +655,7 @@ class SkillScraper(BaseScraper):
                 skill_desc_en = skill["desc_en"]
                 skill_iconid = skill["iconid"]
                 skill_rarity = skill["rarity"]
+                skill_condition = skill.get("condition_groups", [{}])[0].get("condition", "")
                 skill_inherited = False
                 skill_cost = skill.get("cost", None)
                 # For inherited unique skills, we actually want the
@@ -665,6 +666,7 @@ class SkillScraper(BaseScraper):
                     skill_desc_en = skill["gene_version"]["desc_en"]
                     skill_iconid = skill["gene_version"]["iconid"]
                     skill_rarity = skill["gene_version"]["rarity"]
+                    skill_condition = skill["gene_version"].get("condition_groups", [{}])[0].get("condition", "")
                     skill_inherited = skill["gene_version"].get("inherited", False)
                     skill_cost = skill["gene_version"].get("cost", None)
 
@@ -707,6 +709,7 @@ class SkillScraper(BaseScraper):
                     "eval_pt": extra_data["evaluation_points"],
                     "pt_ratio": extra_data["point_ratio"],
                     "rarity": skill_rarity,
+                    "condition": skill_condition,
                     "inherited": skill_inherited,
                     "community_tier": community_tier,
                     "versions": sorted(skill.get("versions", [])),
